264fc38e2cfa10ba8af5c8bdd7a7b33d05774099
Josep <>
2021-09-09 10:58:38 +0200

    Create first files

---
+++ R/hello.R
@@ -0,0 +1,21 @@
+#' @title Dibuja el histograma de una variable superponiendo la densidad normal ajustada
+#' @description Función que dibuja el histograma de una variable x, superponiendo la densidad normal
+#' ajustada. Si el usuario lo desea puede superponer también un estimador de núcleo de la densidad.
+#' @param x vector de datos cuyo histograma se va a calcular
+#' @param dens valor lógico: TRUE=Superponer estimador de núcleo de la densidad
+#' @return el histograma con la densidad normal superpuesta
+#' @export normalHist
+#' @examples
+#' u=rnorm(1000,100,12)
+#' normalHist(u);
+#' normalHist(u,dens=TRUE)
+#' normalHist(u,dens=TRUE,col="lightcyan")
+
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
---
+++ R/set_scenario.R
@@ -0,0 +1,11 @@
+#' @title Dibuja el histograma de una variable superponiendo la densidad normal ajustada
+#' @description Función que dibuja el histograma de una variable x, superponiendo la densidad normal
+#' ajustada. Si el usuario lo desea puede superponer también un estimador de núcleo de la densidad.
+#' @param x vector de datos cuyo histograma se va a calcular
+#' @param dens valor lógico: TRUE=Superponer estimador de núcleo de la densidad
+#' @return el histograma con la densidad normal superpuesta
+#' @export normalHist
+
+set_scenario <- function(){
+  return("Hello rebuild")
+}
