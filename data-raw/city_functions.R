9da869e313609c1f201865fe92d368443e60b2d2
Josep Pueyo-Ros <56930644+jospueyo@users.noreply.github.com>
2021-09-20 17:58:30 +0200

    Delete data-raw directory

--- data-raw/city_functions.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `city_functions` dataset goes here
-
-city_functions <- read.csv2("C:/Users/jpueyo/Univerza v Ljubljani/EdiCitNet - WP2/citymodels/ediblecity/data-raw/city_functions.csv")
-
-usethis::use_data(city_functions, overwrite = TRUE)
