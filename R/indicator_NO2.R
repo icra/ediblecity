39f53993f77cb745e63fdfba99e529e0c240c33b
Josep <>
2021-09-23 15:11:13 +0200

    no2_seq indicator finished not tested

--- R/indicator_NO2.R
+++ R/indicator_NO2.R
@@ -2,21 +2,21 @@
 #' @description This indicator returns the amount of NO2 that is sequestered by urban green.
 #' @param x An 'sf' object with the urban model of your city and a 'Function' column with categories of
 #' urban features.
-#' @param green_df A dataframe of categories that are considered as urban green with two columns. 'functions'
-#' with the names of 'Function' in 'x' to be considered as green and a 'no2_seq' columns with NO2
-#' sequestration capacity of each function (in ug/s/m2). If NULL, categories and values of 'city_functions' dataset are
-#' considered.
-#' @return If 'return_raster is TRUE, a 'stars' object is returned with NO2 sequestration as attribute. If 'verbose'
-#' is TRUE, a vector with the values of NO2 sequestration. If previous both are FALSE, the summary of values is returned.
-#' Units are ug/s/m2.
+#' @param green_df A dataframe of categories that are considered as urban green with four columns:
+#' \enumerate{
+#'   \item 'functions': Column with the function to be considered in the calculations corresponding
+#'   to 'Function' column in 'x'.
+#'   \item 'no2_seq1': The low range of NO2 sequestration of each function (in ug/s/m2).
+#'   \item 'no2_seq2': The high range of NO2 sequestration of each function (in ug/s/m2).
+#'   \item 'pGreen': The proportion of green surface in each function (0:1).
+#' }
+#' @return A numeric value with the total NO2 sequestration in the city (in grams/second).
 #' @export
 #'
 
+
 no2_seq <- function(x,
-                    green_df = NULL,
-                    traffic_lanes = 'Streets',
-                    max_dist = Inf,
-                    verbose = F
+                    green_df = NULL
                     ){
 
 
@@ -30,13 +30,12 @@ no2_seq <- function(x,
       select(functions, no2_seq1, no2_seq2, pGreen)
   }
 
-  x <- x %>% filter(Function %in% green_df)
+  x <- x %>% filter(Function %in% green_df$functions)
 
 
   x <- left_join(x,green_df, by=c("Function" = "functions"))
   x$pGreen[is.na(x$pGreen)] <- 0
 
-  x$area <- sf::st_area(x)
   x$green_area <- as.numeric(sf::st_area(x)) * x$pGreen
   x$no2_seq <- 0
 
@@ -55,21 +54,8 @@ no2_seq <- function(x,
   }
 
 
-### OLD
+ return(c("grams/second" = sum(x$no2_seq)/1000))
 
-
-  sequest <- stars::st_rasterize(x['no2_seq'], dx=5, dy=5)
-
-  if (return_raster) return(sequest)
-  if (verbose) return (sequest %>% as_tibble() %>% filter(!is.na(no2_seq)) %>% .$no2_seq)
-
-  vector <- sequest %>% as_tibble() %>% filter(!is.na(no2_seq)) %>% .$no2_seq
-  summary_new <- c(summary(vector), "Sum" = sum(vector))
-
-  return(summary_new)
-    c(summary(as_tibble(sequest) %>% filter(!is.na(no2_seq)) %>%  .$no2_seq))
 
 
 }
-
-x %>% select(Function, green_area, no2_seq) %>% filter(Function == "Arable land") %>% mutate(area = sf::st_area(.))
